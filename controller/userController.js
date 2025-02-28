61aa691d48d780a3c96d5f93f4a0d038a0f98d36
shaniranasinghe <it22371768@my.sliit.lk>
2025-02-28 14:35:59 +0530

    get start to the project
    get start to the project

---
+++ controller/userController.js
@@ -0,0 +1,8 @@
+export const fetch = async (req, res) => {
+    try {
+        return res.json("Hello World");
+    }catch (error) {
+        res.status(500).json({error: "Internal Server error"});
+        
+    }
+}
---
+++ model/userModel.js
@@ -0,0 +1,20 @@
+import mongoosse from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    name:{
+        type:String,
+        required: true
+    },
+    name:{
+        type:String,
+        required: true
+    },
+    name:{
+        type:String,
+        required: true
+    }
+})
